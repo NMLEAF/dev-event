04b6c66cd34c63760b3efaca5aec9ccf782368f0
NMLEAF <nathanmes124@gmail.com>
2026-01-28 17:24:32 +0300

    ✨ feat: update Page component layout and content; integrate ExploreBtn

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import ExploreBtn from "@/components/ExploreBtn";
+
 const Page = () => {
-  return <div>Welcome to first page</div>;
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Cant't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackatons, Meetups, Conferences, all in One Place
+      </p>
+
+      <ExploreBtn />
+    </section>
+  );
 };
 
 export default Page;
