bd8779a214472a66bc2c1d0ff274f81b33438c50
NMLEAF <nathanmes124@gmail.com>
2026-01-28 22:49:01 +0300

    ✨ feat: add events data and update the hero section title

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
+import { events } from "@/lib/constants";
 
 const Page = () => {
   return (
     <section>
       <h1 className="text-center">
-        The Hub for Every Dev <br /> Event You Cant't Miss
+        The Hub for Every <br />
+        Dev Event You Can't Miss
       </h1>
       <p className="text-center mt-5">
         Hackatons, Meetups, Conferences, all in One Place
@@ -16,8 +19,10 @@ const Page = () => {
         <h3>Featured Events</h3>
 
         <ul className="events">
-          {[1, 2, 3].map((event) => (
-            <li key={event}>Events {event}</li>
+          {events.map((event) => (
+            <li key={event.title} className="list-none">
+              <EventCard {...event} />
+            </li>
           ))}
         </ul>
       </div>
